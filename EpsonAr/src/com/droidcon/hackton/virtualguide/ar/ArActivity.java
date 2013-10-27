d3cbcf100b06ce1e222ceec96a574fc9c90f28c4
Mikolaj Kakol <mkakol@speednet.pl>
2013-10-27 14:59:45 +0100

    automatci location

--- EpsonAr/src/com/droidcon/hackton/virtualguide/ar/ArActivity.java
+++ EpsonAr/src/com/droidcon/hackton/virtualguide/ar/ArActivity.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.util.TypedValue;
@@ -31,9 +32,10 @@ import com.droidcon.hackton.virtualguide.R;
 import com.droidcon.hackton.virtualguide.info.InfoActivity;
 import com.droidcon.hackton.virtualguide.util.DistanceFormatter;
 import com.droidcon.hackton.virtualguide.util.LocationProvider;
+import com.droidcon.hackton.virtualguide.util.LocationProvider.OnLocationUpdated;
 import com.squareup.picasso.Picasso;
 
-public class ArActivity extends Activity {
+public class ArActivity extends Activity implements OnLocationUpdated {
 
 	private LocationProvider locationProvider;
 	private ArView arView;
@@ -48,30 +50,33 @@ public class ArActivity extends Activity {
 			@Override
 			public void isDone(List<POI> resultObject,
 					ApiomatRequestException exception) {
-				poiAdapter.clear();
-				for (POI serverPoi : resultObject) {
-					Poi p = new Poi(LocationProvider.generateLocation(
-							serverPoi.getLocationLatitude(),
-							serverPoi.getLocationLongitude()));
-					p.setName(serverPoi.getName());
-					p.setImageUrl(serverPoi.getImageURL());
+				if (resultObject != null) {
+					poiAdapter.clear();
+					for (POI serverPoi : resultObject) {
+						Poi p = new Poi(LocationProvider.generateLocation(
+								serverPoi.getLocationLatitude(),
+								serverPoi.getLocationLongitude()));
+						p.setName(serverPoi.getName());
+						p.setImageUrl(serverPoi.getImageURL());
 
-					Resources r = getResources();
-					p.setOffsetLeft((int) TypedValue.applyDimension(
-							TypedValue.COMPLEX_UNIT_DIP, 32,
-							r.getDisplayMetrics()));
-					p.setOffsetTop((int) TypedValue.applyDimension(
-							TypedValue.COMPLEX_UNIT_DIP, 80,
-							r.getDisplayMetrics()));
+						Resources r = getResources();
+						p.setOffsetLeft((int) TypedValue.applyDimension(
+								TypedValue.COMPLEX_UNIT_DIP, 32,
+								r.getDisplayMetrics()));
+						p.setOffsetTop((int) TypedValue.applyDimension(
+								TypedValue.COMPLEX_UNIT_DIP, 80,
+								r.getDisplayMetrics()));
 
-					poiAdapter.add(p);
+						poiAdapter.add(p);
+					}
+					poiAdapter.notifyDataSetChanged();
 				}
-				poiAdapter.notifyDataSetChanged();
 			}
 		});
 
 		poiAdapter = new PoiAdapter(this);
-		locationProvider = new LocationProvider();
+		locationProvider = new LocationProvider(
+				(LocationManager) getSystemService(Context.LOCATION_SERVICE));
 
 		Button changeModeButton = (Button) findViewById(R.id.toMapButton);
 		changeModeButton.setOnClickListener(new OnClickListener() {
@@ -87,7 +92,6 @@ public class ArActivity extends Activity {
 		});
 
 		arView = (ArView) findViewById(R.id.ar_view);
-		arView.setUserPosition(locationProvider.getLocation());
 		arView.setAdapter(poiAdapter);
 		arView.setOnPoiClickListener(new OnPoiClickListener() {
 
@@ -106,12 +110,14 @@ public class ArActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		arView.startAr();
+		locationProvider.addOnLocationListener(this);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 		arView.stopAr();
+		locationProvider.removeOnLocationListener(this);
 	}
 
 	private class PoiAdapter extends ArArrayAdapter<Poi> {
@@ -164,5 +170,10 @@ public class ArActivity extends Activity {
 			this.imageUrl = imageUrl;
 		}
 
+	}
+
+	@Override
+	public void onLocationUpdated(Location location) {
+		arView.setUserPosition(location);
 	}
 }
--- EpsonAr/src/com/droidcon/hackton/virtualguide/util/LocationProvider.java
+++ EpsonAr/src/com/droidcon/hackton/virtualguide/util/LocationProvider.java
@@ -1,15 +1,67 @@
 package com.droidcon.hackton.virtualguide.util;
 
-import android.location.Location;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class LocationProvider {
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class LocationProvider implements LocationListener {
+
+	public interface OnLocationUpdated {
+
+		void onLocationUpdated(Location location);
+	}
+
+	private LocationManager locationManager;
+	private ArrayList<OnLocationUpdated> callbacks = new ArrayList<LocationProvider.OnLocationUpdated>();
+	private Location currentLocation;
+
+	public LocationProvider(LocationManager locationManager) {
+		this.locationManager = locationManager;
+	}
+
+	public void addOnLocationListener(OnLocationUpdated listener) {
+		callbacks.add(listener);
+		if (callbacks.size() == 1) {
+			startFetchingLocation();
+		}
+	}
+
+	public void removeOnLocationListener(OnLocationUpdated listener) {
+		callbacks.remove(listener);
+		if (callbacks.size() == 0) {
+			stopFetchingLocation();
+		}
+	}
+
+	private void startFetchingLocation() {
+		for (String locProvider : locationManager.getAllProviders()) {
+			if (locationManager.isProviderEnabled(locProvider)) {
+				if (LocationComparator.isBetterLocation(locationManager.getLastKnownLocation(locProvider),
+						currentLocation)) {
+					currentLocation = locationManager.getLastKnownLocation(locProvider);
+					fireLocationChanged();
+				}
+			}
+		}
+		
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				3000, 0, this);
+	}
+
+	private void stopFetchingLocation() {
+		locationManager.removeUpdates(this);
+	}
 
 	// 51.517682, -0.121287
 	// 51.517715, -0.120912
 
-	public Location getLocation() {
-		return generateLocation(51.517715, -0.120912);
-	}
+//	public Location getLocation() {
+//		return generateLocation(51.517715, -0.120912);
+//	}
 
 	public static Location generateLocation(double lat, double lon) {
 		return generateLocation(lat, lon, 0);
@@ -22,4 +74,108 @@ public class LocationProvider {
 		l.setAltitude(alt);
 		return l;
 	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		if (LocationComparator.isBetterLocation(location, currentLocation)) {
+			currentLocation = location;
+			fireLocationChanged();
+		}
+	}
+
+	private void fireLocationChanged() {
+		for (OnLocationUpdated callback : callbacks) {
+			callback.onLocationUpdated(currentLocation);
+		}
+	}
+
+	private static class LocationComparator {
+
+		private static final int TWO_MINUTES = 1000 * 60 * 30;
+
+		/**
+		 * Determines whether one Location reading is better than the current
+		 * Location fix
+		 * 
+		 * @param location
+		 *            The new Location that you want to evaluate
+		 * @param currentBestLocation
+		 *            The current Location fix, to which you want to compare the
+		 *            new one
+		 */
+		public static boolean isBetterLocation(Location location,
+				Location currentBestLocation) {
+			if (location == null) {
+				return false;
+			}
+			long timeDiff = new Date().getTime() - location.getTime();
+			if ((timeDiff > TWO_MINUTES) || (timeDiff < -TWO_MINUTES)) {
+				return false;
+			}
+			if (currentBestLocation == null) {
+				return true;
+			}
+
+			// Check whether the new location fix is newer or older
+			long timeDelta = location.getTime() - currentBestLocation.getTime();
+			boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+			boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+			boolean isNewer = timeDelta > 0;
+
+			// If it's been more than two minutes since the current location,
+			// use the new location because the user has likely moved
+			if (isSignificantlyNewer) {
+				return true;
+				// If the new location is more than two minutes older, it must
+				// be worse
+			} else if (isSignificantlyOlder) {
+				return false;
+			}
+
+			// Check whether the new location fix is more or less accurate
+			int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation
+					.getAccuracy());
+			boolean isLessAccurate = accuracyDelta > 0;
+			boolean isMoreAccurate = accuracyDelta < 0;
+			boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+			// Check if the old and new location are from the same provider
+			boolean isFromSameProvider = isSameProvider(location.getProvider(),
+					currentBestLocation.getProvider());
+
+			// Determine location quality using a combination of timeliness and
+			// accuracy
+			if (isMoreAccurate) {
+				return true;
+			} else if (isNewer && !isLessAccurate) {
+				return true;
+			} else if (isNewer && !isSignificantlyLessAccurate
+					&& isFromSameProvider) {
+				return true;
+			}
+			return false;
+		}
+
+		/** Checks whether two providers are the same */
+		private static boolean isSameProvider(String provider1, String provider2) {
+			if (provider1 == null) {
+				return provider2 == null;
+			} else {
+				return provider1.equals(provider2);
+			}
+		}
+	}
+
 }
